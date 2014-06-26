cff6b14627c265ee87bfc01fb48f0c6d6e5aa70f
jacobedawson <reviber8@gmail.com>
2014-06-27 01:45:35 +0200

    Continued working on Total Search function
    on the way, current probs with giphy as a blocker

--- masonry-script.js
+++ masonry-script.js
@@ -36,6 +36,22 @@ $closeEditBox.on('click', function() {
 	counter = 15;
 	console.log(counter);
 });
+
+function noGifAlert() {
+	$('body').prepend('<div class="alert alert-warning alert-dismissible" role="alert">' +
+  								 '<button type="button" class="close" data-dismiss="alert">' +
+  								 '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>' +
+  								 '</button><strong>No Results Found!</strong></div>');
+};
+
+function endOfTheLine() {
+	$('body').prepend('<div class="alert alert-warning alert-dismissible" role="alert">' +
+  								 '<button type="button" class="close" data-dismiss="alert">' +
+  								 '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>' +
+  								 '</button><strong>End of the Line!</strong></div>');
+};
+
+
 	
 
 //create array to gather all of the gifs
@@ -54,44 +70,118 @@ $gif_search.on('submit', function(event) {
 	var tag = ($gifPullSubmit.val());
 	//submit the query to giphy first
 	var getGif = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=dc6zaTOxFJmzC&limit=100";
-	//submit a json query to giphy
-	$.getJSON(getGif, function(gifs) {
-		for (var i = 0; i < 100; i++) {
-			if(gifs.data[i] === undefined) {
-				$gifArea.append('<div class="alert alert-warning alert-dismissible" role="alert">' +
-  								 '<button type="button" class="close" data-dismiss="alert">' +
-  								 '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>' +
-  								 '</button><strong>No Results Found!</strong></div>');
-				return;
-			} else {
-				allTheGifs.push('<div class="item"><img src=\"' + gifs.data[i].images.fixed_height.url + '\"\/></div>');
-			}
 
-				
+	//submit a json query to giphy
+$.getJSON(getGif, function(gifs) {
+				if (gifs.data.length == 0) {
+						console.log('giphy aint got it');
+						return;
+						} else {
+							console.log('giphy got it');
+							for(var i = 0; i < gifs.data.length; i++) {
+							allTheGifs.push('<div class="item"><img src=\"' + gifs.data[i].images.fixed_height.url + '\"\/></div>');
+				              	}
+							}
+			//END GIPHY QUERY 
+
+
+			
+							      
+
+
 								
 		
-						}
+						
+
+	//submit a query to fuck yeah gifs
+	$.ajax({
+			url: "http://api.tumblr.com/v2/blog/fuckyeahreactions.tumblr.com/posts/" +
+			"?api_key=9Yt8UYwcFzmGT08az8NmSMVYsEGQwnVWio1dbUuJ4d4mZsp9kr&tag=" + tag,
+						        dataType: 'jsonp',
+						        success: function(data){
+						            if(data.response.posts.length == 0) {
+						            	console.log('fuck yeah aint got it');
+					                	return;
+					                } else {
+					                	console.log('fuck yeah got it');
+					                	for(var i = 0; i < data.response.posts.length; i++) {
+						                    var gif = data.response.posts[i].body;
+						                    var gifSrc = $(gif).find('img').attr('src');
+						                    allTheGifs.push('<div class="item"><img src=\"' + gifSrc + '\"\/></div>');
+						                }  
+					                }
+						                                         
+						        }
+						    });		
+			//END FUCK YEAH QUERY
+
+	//REPLY GIF FUNCTION
+	var getGif = "http://replygif.net/api/gifs?tag=" + tag + "&tag-operator=and&api-key=39YAprx5Yi";
+
+								$.getJSON(getGif, function(gifs) {
+									if (gifs.length == 0) {
+									console.log('reply gif aint got it');
+									return;
+								} else {
+									console.log('reply gif got it');
+									for(var i = 0; i < gifs.length; i++) {
+									allTheGifs.push('<div class="item"><img src=\"' +gifs[i].file + '\"\/></div>');
+				              			}
+									}
+							      
+				     			});
+	//END REPLY GIF FUNCTION
+
+	//GIFBASE FUNCTION WITH PAGINATION CONTROL
+	tag = tag.replace(/\s+/g, '');
+	var getGif = "http://www.gifbase.com/tag/" + tag + "?p=1&format=json";
+							//function if empty, display message
+							$.getJSON(getGif, function(data) {
+								if(data.length == 0 || data.error == "no gifs found for specified tag") {
+									console.log('Gifbase aint got it');
+									return;
+								} else {
+								console.log('Gifbase got it');
+								for(var j = 1; j <= data.page_count; j++) {
+									getGif = "http://www.gifbase.com/tag/" + tag + "?p=" + j + "&format=json";
+									$.getJSON(getGif, function(data) {
+										for(var i = 0; i < data.gifs.length; i++) {
+										allTheGifs.push('<div class="item"><img src=\"' + data.gifs[i].url + '\"\/></div>');
+												}
+											});
+										}
+									}
+								});					
+	//END GIFBASE FUNCTION 	
+
+			//BEGIN IMAGES LOADED FUNCTION
 			$gifArea.imagesLoaded( function() {
+			console.log('all the gifs length is ' + allTheGifs.length);
 			for(var j = 0; j < 15; j++) {
-				$gifArea.append(allTheGifs[j]).masonry();
+				if(allTheGifs.length === 0) {
+					noGifAlert();
+				} else {
+					$gifArea.append(allTheGifs[j]).masonry();
+					}
+				
 				}
+				
 			});
+			//END IMAGES LOADED FUNCTION
+			
 		});
-		
-	
+		//END GIPHY FUNCTION
+
 });
+//END GIF SEARCH	
 
 
-
-//all the gifs now contains all the gifs loaded
+//SHOW NEXT GIFS FUNCTION
 $nextGifs.on('click', function() {
 	console.log('all the gifs length is ' + allTheGifs.length);
 	if((allTheGifs.length - 1) < counter) {
-		$gifArea.append('<div class="alert alert-warning alert-dismissible" role="alert">' +
-  								 '<button type="button" class="close" data-dismiss="alert">' +
-  								 '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>' +
-  								 '</button><strong>End of the Line!</strong></div>');
-		return
+		endOfTheLine();
+		return;
 	};
 
 	console.log(allTheGifs);
@@ -109,9 +199,9 @@ $nextGifs.on('click', function() {
 			
 			console.log(counter);
 		});
+//END NEXT GIFS FUNCTION
 
-
-//PREVIOUS FUNCTION
+//SHOW PREVIOUS GIFS FUNCTION
 $previousGifs.on('click', function() {
 	console.log('all the gifs length is ' + allTheGifs.length);
 	if(counter < 0) {
@@ -137,6 +227,8 @@ $previousGifs.on('click', function() {
 			
 			console.log(counter);
 		});
+//END PREVIOUS GIFS FUNCTION
 
 
 });
+//END SCRIPT
