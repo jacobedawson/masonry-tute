4ad132bf0e95ab447bca42752fac31e475185caa
jacobedawson <reviber8@gmail.com>
2014-06-26 02:44:17 +0200

    One of those things
    Was going well, then it was 2am and all of a sudden something fucking
    broke seemingly spontaneously. Absolutely no idea. Fuck it :)

--- masonry-script.js
+++ masonry-script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-	var $container = $('#gifSearchOverlay');
+
+var $gifPullSubmit = $('#gifPullSubmit');
+var $gif_search = $('#gif_search');
+var $gifArea = $('#gifArea');
+var $container = $('#gifSearchOverlay').masonry();
+
 		// initialize
 	$container.imagesLoaded(function() {
 		$container.masonry({
@@ -21,24 +26,26 @@ $('#getGifs').on('click', function() {
 
 $('#hideGifs').on('click', function() {
 	$container.hide();
+	$('#gifArea').html('');
 });
 	
-var $gifPullSubmit = $('#gifPullSubmit');
-var $gif_search = $('#gif_search');
+
 
 
 $gif_search.on('submit', function(event) {
+	$('#gifArea').html('');
 	var allTheGifs = [];
 	event.preventDefault();
 	var tag = ($gifPullSubmit.val());
-	var getGif = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=dc6zaTOxFJmzC&limit=30";
+	var getGif = "http://api.giphy.com/v1/gifs/search?q=" + tag + "&api_key=dc6zaTOxFJmzC&limit=15";
 	$.getJSON(getGif, function(gifs) {
 		for (var i = 0; i < 15; i++) {
 		allTheGifs.push('<div class="item"><img src=\"' + gifs.data[i].images.fixed_height.url + '\"\/></div>');
 					}
-			$('#gifArea').append(allTheGifs).masonry();
+			$container.imagesLoaded( function() {
+				$('#gifArea').append(allTheGifs).masonry();
 			});
-
+		});
 });
 
 
